ddfca7ba9e6ff5b35e38c5419227cb5d4aa44ee0
Aditya Rawal <adityarawal124@gmail.com>
2023-10-21 01:00:28 +0530

    Second commit as selection  sort

--- sorting_college.java
+++ sorting_college.java
@@ -1,4 +1,6 @@
 public class sorting_college {
+    // BUBBLE SORT
+
     public static void Bubble_sort(int number[]) {
         for (int i = 0; i < number.length - 1; i++) { // TURNS
             for (int j = 0; j < number.length - 1 - i; j++) { // swap
@@ -14,8 +16,29 @@ public class sorting_college {
         }
     }
 
+    /* its a second commit for as a SELECTION SORT */
+
+    public static void Selection_sort(int number[]) {
+        for (int i = 0; i < number.length; i++) {
+            int minPos = i;
+            for (int j = i + 1; j < number.length; j++) {
+                if (number[minPos] > number[j]) {
+                    minPos = j;
+                }
+            }
+            // SWAP
+            int temp = number[minPos];
+            number[minPos] = number[i];
+            number[i] = temp;
+        }
+        for (int i = 0; i < number.length; i++) {
+            System.out.print(number[i] + " ");
+        }
+    }
+
     public static void main(String args[]) {
         int number[] = { 1, 473, 32, 21, 5, 49, 77, 41, 66, 44, 52, 344, 87, 91, 152, 88 };
         Bubble_sort(number);
+        Selection_sort(number);
     }
 }

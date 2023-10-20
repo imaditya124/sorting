cfbb85723f8b2c324f51c330c329b8d617384adf
Aditya Rawal <adityarawal124@gmail.com>
2023-10-21 01:24:42 +0530

    fourth commit as inbuilt  sort

--- sorting_college.java
+++ sorting_college.java
@@ -1,3 +1,6 @@
+import java.util.*;
+import java.util.Collection;
+
 public class sorting_college {
     // BUBBLE SORT
 
@@ -52,10 +55,16 @@ public class sorting_college {
         }
     }
 
+    /* INBUILT SORT */
+    public static void Inbuilt_sort(int number[]) {
+        Arrays.sort(number);
+    }
+
     public static void main(String args[]) {
         int number[] = { 1, 473, 32, 21, 5, 49, 77, 41, 66, 44, 52, 344, 87, 91, 152, 88 };
         Bubble_sort(number);
         Selection_sort(number);
         INsertion_Sort(number);
+        Inbuilt_sort(number);
     }
 }
